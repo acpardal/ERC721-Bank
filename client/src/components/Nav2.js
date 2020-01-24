e3cc4d6ba5e4c5f75225a0ab8bca2421a69320a1
Jack Watters <squawfreeskier@gmail.com>
2020-01-24 13:33:05 -0800

    updated webpage

---
+++ client/src/components/Nav2.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import Grid from "@material-ui/core/Grid";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+class ButtonAppBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  handleScroll = () => {
+    if (
+      document.body.scrollTop > 10 ||
+      document.documentElement.scrollTop > 10
+    ) {
+      var styleHold = document.getElementById("nav");
+      styleHold.style.background = "white";
+      styleHold.style.paddingTop = "15px";
+    } else {
+      var styleHold1 = document.getElementById("nav");
+      styleHold1.style.background = "transparent";
+      styleHold1.style.paddingTop = "15px";
+    }
+  };
+
+  componentDidMount = () => {
+    // console.log(document.getElementById("wallet"));
+    window.addEventListener("scroll", this.handleScroll);
+    var doc = document.getElementById("wallet").textContent;
+    var str = doc.match(/.{1,6}/g);
+    var fin = str[0] + "..." + str[str.length - 1];
+    document.getElementById("wallet").textContent = fin;
+  };
+  handleOpen = () => {
+    this.setState({
+      open: true
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+  render() {
+    const { address, balance } = this.props;
+    return (
+      <div>
+        <AppBar
+          id="nav"
+          position="fixed"
+          style={{ background: "none", boxShadow: "none" }}
+        >
+          <Toolbar>
+            <Grid container>
+              <Grid item xs={6}>
+                <Button
+                  variant="outlined"
+                  className="buttonStyle1"
+                  id="wallet"
+                  onClick={this.handleOpen}
+                >
+                  {address}
+                </Button>
+              </Grid>
+              <Grid item xs={6}>
+                <img
+                  src="LogoRocket.png"
+                  alt="ok"
+                  className="imgStyle1"
+                  style={{ height: "35px" }}
+                />
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+        <Dialog
+          onClose={this.handleClose}
+          aria-labelledby="simple-dialog-title"
+          open={this.state.open}
+        >
+          <div className="divStyle1" style={{ padding: "20px" }}>
+            <p className="spanStyle1">Address:</p>
+            <p className="pStyle4">{address}</p>
+            <p className="spanStyle1">Balance: </p>
+            <p className="pStyle2">{balance}</p>
+          </div>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default ButtonAppBar;
